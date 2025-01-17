c3b568bcf404529e558a5d42c69e0eda366392ed
Alexandra Staszewski <alexandra_sta@yahoo.de>
2025-01-17 22:24:44 +0100

    NavBar Adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import NavBar from './components/NavBar';
 import { Outlet } from 'react-router-dom';
 
 
 function App () {
   return (
     <div className="App">
+      <NavBar />
       <main className="main_container">
         
         <Outlet />
